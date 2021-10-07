3c73080f8d23cb6a4163e42720d376615d127c64
Patrick Coxall <mr.coxall@mths.ca>
2021-10-07 14:21:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 xrandr --output HDMI-0 --mode 3840x2160 --rate 60
+
 sudo apt -y install shotwell
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+sudo snap install -y code --classic
+
+sudo snap install -y arduino
