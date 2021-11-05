54c3db4715101cb2714fa6c97c91b801ec94caaf
Patrick Coxall <mr.coxall@mths.ca>
2021-11-05 09:17:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-xrandr --output HDMI-0 --mode 3840x2160 --rate 60
+xrandr --output HDMI-1 --mode 3840x2160 --rate 60
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
-sudo snap install -y code --classic
+sudo snap install code --classic
 
-sudo snap install -y arduino
+sudo snap install arduino
 
-sudo snap install -y gimp
+sudo snap install gimp
 
 sudo snap install tiled
 
