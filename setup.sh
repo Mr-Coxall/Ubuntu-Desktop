d396a24b0f32c11e2233ec85c16fff1731fc1b8f
Patrick Coxall <mr.coxall@mths.ca>
2021-11-05 11:23:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 xrandr --output HDMI-1 --mode 3840x2160 --rate 60
 
+sudo apt install lightdm
+sudo sh -c 'printf "[Seat:*]\nallow-guest=true\n" > /etc/lightdm/lightdm.conf.d/40-enable-guest.conf'
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
