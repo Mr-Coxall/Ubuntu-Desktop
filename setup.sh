bc1a56c9b8ecf99696a80e47ef7a28afe28afa88
Patrick Coxall <mr.coxall@mths.ca>
2021-11-05 11:49:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 sudo apt install -y rdesktop
-
 sudo apt install -y lightdm
 sudo sh -c 'printf "[Seat:*]\nallow-guest=true\n" > /etc/lightdm/lightdm.conf.d/40-enable-guest.conf'
 
@@ -7,11 +6,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
 sudo snap install code --classic
-
 sudo snap install arduino
-
 sudo snap install gimp
-
 sudo snap install tiled
-
 sudo snap install mu-editor --edge
