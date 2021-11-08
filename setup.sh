c70ab44f4fab6d58ff950d5ce38c8aae870c180c
Patrick Coxall <mr.coxall@mths.ca>
2021-11-08 08:39:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 sudo apt-get install -y freerdp2-x11
+
 sudo apt install -y lightdm
 sudo sh -c 'printf "[Seat:*]\nallow-guest=true\n" > /etc/lightdm/lightdm.conf.d/40-enable-guest.conf'
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+https://downloads.raspberrypi.org/imager/imager_latest_amd64.deb
+sudo apt install -y ./imager_latest_amd64.deb
 
 sudo snap install code --classic
 sudo snap install arduino
